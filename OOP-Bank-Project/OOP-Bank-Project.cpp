8338fdace3187a576ba9264f342a4d07f575be2c
Ahmad <ahmadalkurdi9012@gmail.com>
2026-09-17 12:02:25 +0300

    Login Tiny Changes

--- OOP-Bank-Project/OOP-Bank-Project.cpp
+++ OOP-Bank-Project/OOP-Bank-Project.cpp
@@ -2,6 +2,10 @@
 #include"clsCurrency.h"
 #include "clsLoginScreen.h"
 
+// When run the code enter those to show the main screen
+// Username: User2
+// Password: 1234
+
 int main()
 {
     while (true)
